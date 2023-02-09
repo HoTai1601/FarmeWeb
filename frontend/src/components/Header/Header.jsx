ca56c4b33ac0eca0839f3acf7939269b04455190
HoTai1601 <hotai1601@gmail.com>
2023-02-09 19:02:57 +0700

    chỉnh sửa giao diện

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,92 +1,91 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux'
-import { LinkContainer } from 'react-router-bootstrap'
-import { Nav, Navbar, NavDropdown, Image } from 'react-bootstrap';
-import './Header.css'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+import { Nav, Navbar, NavDropdown, Image } from "react-bootstrap";
+import "./Header.css";
 
-import { logout } from './../../actions/userActions'
+import { logout } from "./../../actions/userActions";
 
 const Header = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
-    const userLogin = useSelector(state => state.userLogin)
-    const { userInfo } = userLogin
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
 
-    const logoutHandler = () => {
-        dispatch(logout())
-    }
-
-    return (
-        <Navbar collapseOnSelect expand="lg" fixed="top">
-            <LinkContainer to="/">
-                <Navbar.Brand className="nav-cal" >
-                    <Image width="80px"src="/Logo.png" />
-                </Navbar.Brand>
+  return (
+    <Navbar collapseOnSelect expand="lg" fixed="top">
+      <LinkContainer to="/">
+        <Navbar.Brand className="nav-cal">
+          <Image width="100px" src="/Logo1.png" />
+        </Navbar.Brand>
+      </LinkContainer>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto ">
+          <LinkContainer to="/">
+            <Nav.Link className="nav-cal">Trang Chủ</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/farmer">
+            <Nav.Link className="nav-cal">Nông Dân</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/consumer">
+            <Nav.Link className="nav-cal">Khách Hàng</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="login?redirect=supplier">
+            <Nav.Link className="nav-cal">Nhà Cung Cấp</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/cart">
+            <Nav.Link
+              className={`${
+                userInfo ? "remove-space" : "add-space cart nav-cal"
+              } `}
+            >
+              <i className="fas fa-shopping-cart"></i>
+              CART
+            </Nav.Link>
+          </LinkContainer>
+          {userInfo ? (
+            <NavDropdown title={userInfo.name.toUpperCase()} id="username">
+              {userInfo && userInfo.isAdmin && (
+                <LinkContainer to="/admin/dashboard">
+                  <NavDropdown.Item>Dashboard</NavDropdown.Item>
+                </LinkContainer>
+              )}
+              <LinkContainer to="/profile">
+                <NavDropdown.Item>Trang Cá Nhân</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Đăng Xuất
+                </NavDropdown.Item>
+              </LinkContainer>
+            </NavDropdown>
+          ) : (
+            <LinkContainer to="/login">
+              <Nav.Link className="login nav-cal">Đăng Nhập</Nav.Link>
             </LinkContainer>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto ">
-                    <LinkContainer to="/">
-                        <Nav.Link className="nav-cal">HOME</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/farmer">
-                        <Nav.Link className="nav-cal">FARMER</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/consumer">
-                        <Nav.Link className="nav-cal">CONSUMER</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="login?redirect=supplier">
-                        <Nav.Link className="nav-cal">SUPPLIER</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/cart" >
-                        <Nav.Link className={`${userInfo ? "remove-space" : "add-space cart nav-cal"} `}>
-                            <i className="fas fa-shopping-cart"></i>
-                            CART
-                        </Nav.Link>
-                    </LinkContainer>
-                    {
-                        userInfo ? (
-                            <NavDropdown title={userInfo.name.toUpperCase()} id='username'>
-                                {
-                                    userInfo && userInfo.isAdmin && (
-                                        <LinkContainer to='/admin/dashboard'>
-                                            <NavDropdown.Item>DASHBOARD</NavDropdown.Item>
-                                        </LinkContainer>
-                                    )
-                                }
-                                <LinkContainer to='/profile'>
-                                    <NavDropdown.Item>PROFILE</NavDropdown.Item>
-                                </LinkContainer>
-                                <LinkContainer to='/login'>
-                                    <NavDropdown.Item onClick={logoutHandler}>LOGOUT</NavDropdown.Item>
-                                </LinkContainer>
-                            </NavDropdown>
-                        ) : (
-                                <LinkContainer to="/login">
-                                    <Nav.Link className="login nav-cal">SIGN IN</Nav.Link>
-                                </LinkContainer>
-                            )
-                    }
-                    {
-                        userInfo && userInfo.isAdmin && (
-                            <NavDropdown title="ADMIN" id='adminmenu'>
-                                <LinkContainer to='/admin/userlist'>
-                                    <NavDropdown.Item>USERS</NavDropdown.Item>
-                                </LinkContainer>
-                                <LinkContainer to='/admin/productlist'>
-                                    <NavDropdown.Item>PRODUCTS</NavDropdown.Item>
-                                </LinkContainer>
-                                <LinkContainer to='/admin/orderlist'>
-                                    <NavDropdown.Item>ORDERS</NavDropdown.Item>
-                                </LinkContainer>
-                            </NavDropdown>
-                        )
-                    }
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    )
-}
+          )}
+          {userInfo && userInfo.isAdmin && (
+            <NavDropdown title="ADMIN" id="adminmenu">
+              <LinkContainer to="/admin/userlist">
+                <NavDropdown.Item>Người Dùng</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/admin/productlist">
+                <NavDropdown.Item>Sản Phẩm</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/admin/orderlist">
+                <NavDropdown.Item>Đơn Hàng</NavDropdown.Item>
+              </LinkContainer>
+            </NavDropdown>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
-export default Header
+export default Header;
--- frontend/src/components/Helmet/Meta.jsx
+++ frontend/src/components/Helmet/Meta.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React from "react";
 import { Helmet } from "react-helmet";
 
 const Meta = ({ title, description, keywords }) => {
-    return (
-        <Helmet>
-           <title>{title}</title> 
-           <meta name='description' content={description} />
-           <meta name='keyword' content={keywords} />
-        </Helmet>
-    )
-}
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keyword" content={keywords} />
+    </Helmet>
+  );
+};
 
 Meta.defaultProps = {
-    title: 'Welcome To Agroic',
-    description: 'Agriclutural website by The Department of Agriculture (DoA) of Sri Lanka along with the Keels ',
-    keywords: 'farmers, argio, department of agriculture, agricultural science'
-}
+  title: "Chào mừng đến với Sugoi Ne!",
+  description:
+    "Agriclutural website by The Department of Agriculture (DoA) of Sri Lanka along with the Keels ",
+  keywords: "farmers, argio, department of agriculture, agricultural science",
+};
 
-export default Meta
+export default Meta;
