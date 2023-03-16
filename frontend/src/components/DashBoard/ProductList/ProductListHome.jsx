9c36a3f86757d155397576fc6e461a9091a68526
HoTai1601 <hotai1601@gmail.com>
2023-03-17 03:21:41 +0700

    update giao diện admin3.0

--- frontend/src/components/DashBoard/ProductList/ProductListHome.jsx
+++ frontend/src/components/DashBoard/ProductList/ProductListHome.jsx
@@ -1,27 +1,23 @@
-import React from 'react'
-import {
-    Container,
-    Row,
-} from 'react-bootstrap'
-import './listStyles.css'
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import "./listStyles.css";
 import { Scrollbar } from "react-scrollbars-custom";
-import SeedList from './SeedList/SeedList'
-import LendMachineList from './LendMachineList/LendMachineList'
-import ConsumerList from './ConsumerList/ConsumerList'
+import SeedList from "./SeedList/SeedList";
+import LendMachineList from "./LendMachineList/LendMachineList";
+import ConsumerList from "./ConsumerList/ConsumerList";
 
 const ProductListHome = () => {
+  return (
+    <Scrollbar style={{ width: "100%", height: "80vh" }}>
+      <Container>
+        <Row className="list-container">
+          <SeedList />
+          <LendMachineList />
+          <ConsumerList />
+        </Row>
+      </Container>
+    </Scrollbar>
+  );
+};
 
-    return (
-        <Scrollbar style={{ width: '100%', height: 450 }}>
-            <Container>
-                <Row className="list-container">
-                    <SeedList />
-                    <LendMachineList />
-                    <ConsumerList />
-                </Row>
-            </Container>
-        </Scrollbar>
-    )
-}
-
-export default ProductListHome
+export default ProductListHome;
